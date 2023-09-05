54b6cf1e5aef4f76707eaa7ad940bcc92733b17b
Levy <levymcruz@gmail.com>
2023-09-05 20:46:06 -0300

    apagando alguns arquivos antigos

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,15 +1,10 @@
 import { Router } from "express";
 
-import authRouter from "./auth.routes.js";
-import usersRouter from "./users.routes.js";
 import passengersRouter from "./passengers.routes.js";
-import postRouter from "./post.routes.js";
 
 const router = Router();
 
-router.use(authRouter);
-router.use(usersRouter);
 router.use(passengersRouter);
-router.use(postRouter);
+
 
 export default router;
