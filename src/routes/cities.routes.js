45bac514173d2d8ed63846f74b2c14500104242c
Levy <levymcruz@gmail.com>
2023-09-05 21:05:19 -0300

    feat: adding citySchema

--- src/routes/cities.routes.js
+++ src/routes/cities.routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import CitiesController from "../controllers/cities.controller.js";
 import { validateSchema } from "../middlewares/validateSchema.js";
-import { passengerSchema } from "../schemas/passengers.schemas.js";
+import { citySchema } from "../schemas/cities.schemas.js";
 
 const citiesRouter = Router();
 const controller = new CitiesController()
 
-citiesRouter.post('/cities', controller.handlePostPassenger); //, validateSchema(passengerSchema)
+citiesRouter.post('/cities', validateSchema(citySchema), controller.handlePostPassenger); //, 
 
 export default citiesRouter;
